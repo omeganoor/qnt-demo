623a1246e55c30b05fa11556e7ac6a72e0400932
Nur Hakim Soleh <32730142+omeganoor@users.noreply.github.com>
2024-04-03 20:06:00 +0700

    Add files via upload
    initial commit

---
+++ src/test/java/com/qnt/assigment/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnt.assigment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
